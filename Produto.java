b27adf7aa1d967c1770fce782f09497b9dbb30e9
Demetrius <demetrius.mp789@gmail.com>
2021-05-21 20:09:54 -0400

    Adicionando mais um construtor, id com auto increment, e atributos adicionais

--- Produto.java
+++ Produto.java
@@ -1,15 +1,30 @@
 public abstract class Produto{
+    private static int contId = 0;
     private int id;
     private String nome;
-    private float valor;
+    private float valorDeCompra;
+    private float valorDeVenda;
     private int qtdEstoque;
+    private int qtdVendido;
     private float imposto;
 
-    public Produto(int id, String nome, float valor, int qtdEstoque, float imposto){
+    public Produto(String nome, float valorDeCompra, float valorDeVenda, int qtdEstoque, int qtdVendido, float imposto){
+        this.id = Produto.contId++;
+        this.nome = nome;
+        this.valorDeCompra = valorDeCompra;
+        this.valorDeVenda = valorDeVenda;
+        this.qtdEstoque = qtdEstoque;
+        this.qtdVendido = qtdVendido;
+        this.imposto = imposto;
+    }
+
+    public Produto(int id, String nome, float valorDeCompra, float valorDeVenda, int qtdEstoque, int qtdVendido, float imposto){
         this.id = id;
         this.nome = nome;
-        this.valor = valor;
+        this.valorDeCompra = valorDeCompra;
+        this.valorDeVenda = valorDeVenda;
         this.qtdEstoque = qtdEstoque;
+        this.qtdVendido = qtdVendido;
         this.imposto = imposto;
     }
 
@@ -21,16 +36,32 @@ public abstract class Produto{
         return this.nome;
     }
 
+    public float getValorDeCompra() {
+        return valorDeCompra;
+    }
+
+    public void setValorDeCompra(float valorDeCompra) {
+        this.valorDeCompra = valorDeCompra;
+    }
+
+    public float getValorDeVenda() {
+        return valorDeVenda;
+    }
+
+    public void setValorDeVenda(float valorDeVenda) {
+        this.valorDeVenda = valorDeVenda;
+    }
+
+    public int getQtdVendido() {
+        return qtdVendido;
+    }
+
+    public void setQtdVendido(int qtdVendido) {
+        this.qtdVendido = qtdVendido;
+    }
+
     public void setNome(String nome){
         this.nome = nome;
-    }
-
-    public float getValor(){
-        return this.valor;
-    }
-
-    public void setValor(float valor){
-        this.valor = valor;
     }
 
     public int getQtdEstoque(){
